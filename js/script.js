69d79f18326dda8e448231eecc59400aae79eeea
MrTonyJohns <143044602+MrTonyJohns@users.noreply.github.com>
2023-10-15 13:38:20 -0500

    Changed some changes

--- js/script.js
+++ js/script.js
@@ -57,80 +57,60 @@ $(function () {
   });
 })();
 
-/* Particles settings */
-/* tsParticles.load("tsparticles", {
-  fps_limit: 60,
-  interactivity: {
-    detect_on: "window",
-    events: {
-      onclick: { enable: true, mode: "push" },
-      onhover: {
-        enable: true,
-        mode: "attract",
-        parallax: { enable: false, force: 60, smooth: 10 },
-      },
-      resize: true,
-    },
-    modes: {
-      push: { quantity: 4 },
-      attract: { distance: 200, duration: 0.4, factor: 5 },
-    },
-  },
-  particles: {
-    color: { value: "#222222" },
-    line_linked: {
-      color: "#222222",
-      distance: 150,
-      enable: true,
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      attract: { enable: false, rotateX: 600, rotateY: 1200 },
-      bounce: false,
-      direction: "none",
-      enable: true,
-      out_mode: "out",
-      random: false,
-      speed: 2,
-      straight: false,
-    },
-    number: { density: { enable: true, value_area: 800 }, value: 80 },
-    opacity: {
-      anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
-      random: false,
-      value: 0.5,
-    },
-    shape: {
-      character: {
-        fill: false,
-        font: "Verdana",
-        style: "",
-        value: "*",
-        weight: "400",
-      },
-      image: {
-        height: 100,
-        replace_color: true,
-        src: "images/github.svg",
-        width: 100,
-      },
-      polygon: { nb_sides: 5 },
-      stroke: { color: "#000000", width: 0 },
-      type: "circle",
-    },
-    size: {
-      anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
-      random: true,
-      value: 5,
-    },
-  },
-  polygon: {
-    draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
-    move: { radius: 10 },
-    scale: 1,
-    type: "none",
-    url: "",
-  },
-  retina_detect: true,
-}); */
+// $("img").on("click", function () {
+//   $("#image-one").animate(
+//     {
+//       opacity: 0.5,
+//     },
+//     1500
+//   );
+// });
+$(document).ready(function ($) {
+  $(".lightbox-trigger").click(function (e) {
+    //prevent default action (hyperlink)
+    e.preventDefault();
+
+    //Get clicked link href
+    let image_href = $(this).attr("href");
+
+    /* 	
+		If the lightbox window HTML already exists in document, 
+		change the img src to to match the href of whatever link was clicked
+		
+		If the lightbox window HTML doesn't exists, create it and insert it.
+		(This will only happen the first time around)
+		*/
+
+    if ($("#lightbox").length > 0) {
+      // #lightbox exists
+
+      //place href as img src value
+      $("#content").html('<img src="' + image_href + '" />');
+
+      //show lightbox window - you could use .show('fast') for a transition
+      $("#lightbox").show();
+    } else {
+      //#lightbox does not exist - create and insert (runs 1st time only)
+
+      //create HTML markup for lightbox window
+      let lightbox =
+        '<div id="lightbox">' +
+        "<p>Close</p>" +
+        '<div id="content">' + //insert clicked link's href into img src
+        '<img src="' +
+        image_href +
+        '" />' +
+        "</div>" +
+        "</div>";
+
+      //insert lightbox HTML into page
+      $("body").append(lightbox);
+    }
+  });
+
+  //Click anywhere on the page to get rid of lightbox window
+  $("body").on("click", "#lightbox", function () {
+    //must use on, as the lightbox element is inserted into the DOM
+    $("#lightbox").hide();
+  });
+});
